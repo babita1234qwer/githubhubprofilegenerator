3890db0e17d88a72eeb0595a3a24a1b114365901
babita patel <babita1234qwer@gmail.com>
2025-06-21 02:26:07 +0530

    Initial commit

---
+++ app.js
@@ -0,0 +1,14 @@
+import React from "react";
+import ReactDOM from "react-dom/client"
+import Header from "../component1/header";
+import Body from "../component1/body";
+
+function GithubProfile(){
+
+    return(<>
+    <Header/>
+    <Body/>
+    </>)
+}
+const root=ReactDOM.createRoot(document.getElementById("root"));
+root.render(<GithubProfile/>)
---
+++ component1/body.js
@@ -0,0 +1,48 @@
+import React,{useState,useCallback,useEffect} from "react";
+import ReactDOM from "react-dom/client";
+
+
+
+
+
+function Body(){
+    const [Profile,setProfile]=useState([]);
+    const [numberofprofile,setnumeberofprofile]=useState("");
+    const [name,setname]=useState('');
+    async function generateprofile( num){
+        const ran=Math.floor(1+Math.random()*10000);
+        const response=await fetch(`https://api.github.com/users?since=${ran}&per_page=${num}`);
+        const data=await response.json();
+        const dat=name?data.filter(val=>val.login.toLowerCase()==name.toLowerCase()):data;
+        setProfile(dat);
+
+    }
+    useEffect(()=>{
+        generateprofile(10);
+    },[])
+
+    return (
+   <div className="but">
+    
+    <input type="text" className="input" placeholder="Search Here" value={numberofprofile} onChange={(e)=>setnumeberofprofile(e.target.value)}>
+    </input>
+    <button onClick={()=>generateprofile(Number(numberofprofile))}>Search Profile</button>
+   <div className="profile">
+        {
+            Profile.map((value)=>{
+                return(
+                <div key={value.id} className="cards">
+                    <img src={value.avatar_url}></img>
+                    <h2> {value.login}</h2>
+                    <a href={value.html_url} target="_blank"> Profile </a>
+
+
+                    </div>
+
+             ) })
+        }
+
+    </div> </div>
+    )
+}
+export default Body;
