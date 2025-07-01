4bfec1ead97654d500d0f55c62d9e5bd8bf210bd
babita patel <patelsebu2006@gmail.com>
2025-07-02 04:20:27 +0530

    first commit

--- app.js
+++ app.js
@@ -1,14 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom/client"
-import Header from "../component1/header";
-import Body from "../component1/body";
+import Header from "./component1/header";
+import Body from "./component1/body";
 
 function GithubProfile(){
 
-    return(<>
+    return(
+    <>
     <Header/>
     <Body/>
-    </>)
+    </>
+    );
 }
 const root=ReactDOM.createRoot(document.getElementById("root"));
 root.render(<GithubProfile/>)
